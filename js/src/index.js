74759962edf1c1817f40310a885a1baa6a9ad154
Carlos A <cayala.w@gmail.com>
2023-07-24 11:17:30 -0500

    issue 9: add morgan, jest

--- js/src/index.js
+++ js/src/index.js
@@ -1,12 +1,21 @@
 const express = require("express");
+const morgan = require("morgan");
+const dotenvSafe = require("dotenv-safe");
 
+dotenvSafe.config({
+  path: "./.env",
+});
 const v1WorkoutRouter = require("./v1/routes/workoutRoutes");
 const v1RecordRouter = require("./v1/routes/recordRoutes");
 const v1MemberRouter = require("./v1/routes/memberRoutes");
 
 const app = express();
+const loggerMiddleWare = morgan("dev", {
+  stream: { write: (message) => console.log(message.trim()) },
+});
 const PORT = process.env.PORT || 3000;
 
+app.use(loggerMiddleWare);
 app.use("/api/v1/workouts", v1WorkoutRouter);
 app.use("/api/v1/records", v1RecordRouter);
 app.use("/api/v1/members", v1MemberRouter);
